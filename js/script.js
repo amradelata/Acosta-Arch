17e50a80b7eca73177de6cb8cbcfe7024753d84f
Amr Adel Ata <amradelata@gmail.com>
2018-07-15 16:45:30 +0200

    acosta-arch-tow

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 
-            var containerEl = document.querySelector('.container');
+ //            var containerEl = document.querySelector('.container');
 
-            var mixer = mixitup(containerEl);
+ //            var mixer = mixitup(containerEl);
 
             
 
-	$(window).scroll(function () {
+	// $(window).scroll(function () {
 
-	// scrollTop button
+	// // scrollTop button
 
-		let buttonTop = $('.scroll-top');
-		if($(window).scrollTop()>=2000){
-			buttonTop.fadeIn(500);
-			// console.log('dfds')
-		}else{
-			buttonTop.fadeOut(500);
-		}
-	});
+	// 	let buttonTop = $('.scroll-top');
+	// 	if($(window).scrollTop()>=2000){
+	// 		buttonTop.fadeIn(500);
+	// 		// console.log('dfds')
+	// 	}else{
+	// 		buttonTop.fadeOut(500);
+	// 	}
+	// });
 
-			 $('.scroll-top').click(function () {
+	// 		 $('.scroll-top').click(function () {
 
-			$('html, body').animate({
+	// 		$('html, body').animate({
 
-				scrollTop:0
+	// 			scrollTop:0
 
-			},1000);
+	// 		},1000);
 
-			});
+	// 		});
 
