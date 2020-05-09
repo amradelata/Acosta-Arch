6e402ef75b0d78dc198a74fd3c5ec8688c30a7cb
amradelata <amradelata@gmail.com>
2020-05-09 23:24:26 +0200

    add popup/ modify image size

--- js/script.js
+++ js/script.js
@@ -28,3 +28,21 @@
 
 	// 		});
 
+
+
+	/* popup*/
+let mypopup = document.getElementById('mypopup');
+function closepopup(){
+	mypopup.style.display = "none";
+}
+	function shwpopup() {
+	setTimeout(function(){ 
+
+		mypopup.style.display = "block";
+
+	 }, 3000);
+	}
+
+	shwpopup() 
+
+	/* popup*/
